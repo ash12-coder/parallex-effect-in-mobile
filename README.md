25583ef5b12a07a585a811a6647198dcab16e036
ash12-coder <ashish.agrawal_cs18@gla.ac.in>
2021-06-04 21:52:07 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# parallex-effect-in-mobile
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/parallex-effect-in-mobile)
---
+++ script.js
@@ -0,0 +1,5 @@
+let times = new Date();
+let h = times.getHours();
+let m = times.getMinutes();
+
+document.getElementById('hour').innerHTML = h + ':' + m;
